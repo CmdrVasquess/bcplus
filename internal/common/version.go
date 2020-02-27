3503d6a990f5ee4d7bc838a2e6b1151561ce1971
Vasquess <edtools@sw.fractalqb.de>
2020-02-27 19:13:02 +0100

    improved grid layout

--- internal/common/version.go
+++ internal/common/version.go
@@ -5,6 +5,6 @@ const (
 	BCpMinor   = 8
 	BCpPatch   = 4
 	BCpQuality = `dev`
-	BCpBuildNo = 67
-	BCpDate    = `2020-02-23T13:36:10+01:00`
+	BCpBuildNo = 69
+	BCpDate    = `2020-02-27T19:08:37+01:00`
 )
